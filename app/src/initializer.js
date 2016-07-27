94d13f04d3a1b5bb7ba4c7d2c87722c6f1275547
chanlee <wonchan.lee@gmail.com>
2016-07-27 15:48:04 +0900

    add missing library. velocity

--- app/src/initializer.js
+++ app/src/initializer.js
@@ -2,6 +2,7 @@ window._ = require('underscore');
 window.jquery = window.$ = window.jQuery = require('../bower_components/jquery/dist/jquery.min.js');
 var css = require('../css/fonts.less');
 var css = require('../css/develop.less');
+require('../bower_components/velocity/velocity.min.js');
 
 function init(){
     if(!localStorage.getItem('lang')) {
